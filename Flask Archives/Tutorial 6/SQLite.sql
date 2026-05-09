3957cc9ff6a8dd13b166b7a9325f9e8f0bd6bf53
Dev <devbabbar2014@gmail.com>
2026-05-10 01:04:45 +0530

    Updated folder name

--- Flask Archives/Tutorial 6/SQLite.sql
+++
@@ -1,3 +0,0 @@
--- SQLite
-SELECT * from people;
-insert into people values('2','Tanya',24,'Finance Manager');
--- Flask Archives/Tutorial 7/SQLite.sql
+++
@@ -1,3 +0,0 @@
--- SQLite
-SELECT * from User;
-insert into Users(username, password, role) values ('Dev','Pass','Developer');
